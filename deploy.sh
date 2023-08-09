460adcc6c531160c428054fc2e0ae040e92c537e
Jacob Emery <jacob.emery@ibm.com>
2023-08-09 16:56:08 -0400

    fix: setup script and Ansible playbook
    Signed-off-by: Jacob Emery <jacob.emery@ibm.com>

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
-sudo dnf install python3 -y
-python3 -m pip install ansible
+sudo dnf install python3 redhat-rpm-config gcc libffi-devel python3-devel openssl-devel cargo pkg-config -y
+python3 -m pip3 install cryptography --no-binary --ignore-installed cryptography
+python3 -m pip3 install ansible kubernetes jsonpatch PyYAML --ignore-installed
 ansible-galaxy collection install kubernetes.core
 ansible-playbook yaml/ansible/deploy.yaml
